47150679e2b6b1a5234ce0ee82b64a7e6e93e989
ryorichie <ryorichie@gmail.com>
2022-03-21 19:27:25 +0700

    com

---
+++ Rust/just_lesson/data_types/character_type/target/debug/deps/character_type-2b6925d04daf0ff7.d
@@ -0,0 +1,5 @@
+c:\Users\Ryo Richie\Documents\GitHub\learn\Rust\just_lesson\data_types\character_type\target\debug\deps\character_type-2b6925d04daf0ff7.rmeta: src\main.rs
+
+c:\Users\Ryo Richie\Documents\GitHub\learn\Rust\just_lesson\data_types\character_type\target\debug\deps\character_type-2b6925d04daf0ff7.d: src\main.rs
+
+src\main.rs:
