65e74870528983363fe61567ee86a4999c122241
ryorichie <ryorichie@gmail.com>
2022-03-24 18:06:21 +0700

    fibonaci done

--- Rust/fibonaci/src/main.rs
+++ Rust/fibonaci/src/main.rs
@@ -1,7 +1,27 @@
 fn main() {
-    let num1 = 1;
-    let mut fibonaci = 0;
+    let mut fibonaci: i64 = 0;
+    let mut fibonaci1: i64 = 0;
+    let mut counter: i64 = 0;
     loop {
-        
-    }
+
+        if fibonaci == 0 {
+            println!("{}", fibonaci);
+            fibonaci = 1;
+            println!("{}", fibonaci);
+        }
+        counter += 1;
+        println!("{}", counter);
+
+        fibonaci1 = fibonaci + fibonaci1;
+        println!{"{}",fibonaci1};
+
+        counter += 1;
+        println!("{}", counter);
+
+        fibonaci = fibonaci1 + fibonaci;
+        println!("{}", fibonaci);
+        if fibonaci >= 9223372036854775807 {
+            break
+        }
+    } 
 }
