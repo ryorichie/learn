9884ec8496004ae3af9983abebee38ab63e4a88c
ryorichie <ryorichie@gmail.com>
2022-03-26 06:59:52 +0700

    commit

--- Rust/ownership.rs
+++ Rust/ownership.rs
@@ -194,3 +194,23 @@ fn change(some_string: &mut String) {
 
     println!("{}, {}", r1, r2);
 }
+
+// it's fine if you make scope of it
+let mut s = String::from("hello");
+
+{
+    let r1 = &mut s;
+} // r1 goes out of scope here, so we can make a new reference with no problems.
+
+let r2 = &mut s;
+/**Rust enforces a similar rule for combining mutable 
+ * and immutable references. This code results in an error: */
+fn main() {
+    let mut s = String::from("hello");
+
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    let r3 = &mut s; // BIG PROBLEM
+
+    println!("{}, {}, and {}", r1, r2, r3);
+}
