50ac76805591343fe3d93f7431226e1fea9f40f5
Ryo Richie <ryorichie@gmail.com>
2022-04-09 12:45:00 +0700

    done

--- Java/declaring_variable.java
+++ Java/declaring_variable.java
@@ -3,7 +3,19 @@ public class declaring_variable {
         // Declaring variable
         String firstname = "Ryo";
         String lastname = "Richie";
-        String name = firstname + lastname;
-        System.out.println(name);
+        System.out.println(firstname + lastname);
+
+        int x = 51;
+        int y = 70;
+        System.out.println(x + y);
+    }
+}
+
+public class declaring_variable {
+    public static void main(String[] args) {
+        // Declare Many Variables
+        int x = 5;
+        int y = 6;
+        int z = 20
     }
 }
