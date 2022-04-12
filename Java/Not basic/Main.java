24b44e0b8e85045c48544f89c2217a52e2346b9c
ryorichie <ryorichie@gmail.com>
2022-04-13 06:57:50 +0700

    committing

--- Java/Not basic/Main.java
+++ Java/Not basic/Main.java
@@ -1,3 +1,4 @@
 public class Main {
     int x = 5;
+    int y = 3;
 }
---
+++ Java/Not basic/second.java
@@ -0,0 +1,6 @@
+public class second {
+    public static void main(String[] args) {
+        Main myObj = new Main();
+        System.out.println(myObj.x);
+    }
+}
