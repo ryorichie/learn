323950e90be5d239907a01c623efba7753d1b802
ryorichie <ryorichie@gmail.com>
2022-04-16 11:39:25 +0700

    ciang

---
+++ Java/Not basic/Java Encapsulation/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Person myObj = new Person();
+        myObj.setName("Josh"); // Set the value of the name variable to "Josh"
+        System.out.println(myObj.getName());
+    }
+}
