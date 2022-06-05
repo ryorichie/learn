cf900307dc192daa51106aa4f555a4e43714be6a
ryorichie <ryorichie@gmail.com>
2022-06-05 14:14:24 +0700

    Hedeh

---
+++ R/Tugas/matrix.r
@@ -0,0 +1,8 @@
+matrix <- matrix(c(7, 3, 4, 2, 1, 2, 5, 6, 8, 7, 1, 0, 3, 5, 7, 8), nrow = 4, ncol = 4)
+
+matrix <- matrix(c(2, 3, 5, 7, 11, 13, 17, 19, 23), nrow = 4, ncol = 4)
+matrix
+matrix2 <- matrix(c(2, 3, 5, 7, 11, 13, 17, 19, 23), nrow = 4, ncol = 4)
+matrix2
+matrix3 <- matrix(c(2, 3, 5, 7, 11, 13, 17, 19, 23), nrow = 4, ncol = 4)
+matrix3
