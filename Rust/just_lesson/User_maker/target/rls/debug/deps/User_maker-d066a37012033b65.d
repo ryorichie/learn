fdf0cdf622217bbc4ce55f137365192e3c25097a
ryorichie <ryorichie@gmail.com>
2022-06-21 08:59:06 +0700

    sa

--- Rust/just_lesson/User_maker/target/rls/debug/deps/User_maker-d066a37012033b65.d
+++
@@ -1,5 +0,0 @@
-c:\Users\Ryo Richie\Documents\GitHub\learn\Rust\just_lesson\User_maker\target\rls\debug\deps\User_maker-d066a37012033b65.rmeta: src\main.rs
-
-c:\Users\Ryo Richie\Documents\GitHub\learn\Rust\just_lesson\User_maker\target\rls\debug\deps\User_maker-d066a37012033b65.d: src\main.rs
-
-src\main.rs:
---
+++ Rust/just_lesson/usermaker/target/rls/debug/deps/User_maker-6a14fbd545f8202c.d
@@ -0,0 +1,5 @@
+c:\Users\Ryo Richie\Documents\GitHub\learn\Rust\just_lesson\usermaker\target\rls\debug\deps\User_maker-6a14fbd545f8202c.rmeta: src\main.rs
+
+c:\Users\Ryo Richie\Documents\GitHub\learn\Rust\just_lesson\usermaker\target\rls\debug\deps\User_maker-6a14fbd545f8202c.d: src\main.rs
+
+src\main.rs:
