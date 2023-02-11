e93ca8afe4d16c3c973124943ce577c9d6478cb0
Ryo Richie <88276813+ryorichie@users.noreply.github.com>
2023-02-11 11:53:22 +0700

    logic v2

--- Phyton/logical_operator.py
+++ Phyton/logical_operator.py
@@ -1,6 +1,6 @@
 temp = int(input("What is temperature today ? "))
 
-if temp >= 0 and temp <= 30:
+if not(temp < 0 or temp > 30):
     print("The temperatur is " + str(temp) + "C, have a nice day\ngo outside please")
-else: 
+elif not(temp >= 0 and temp <= 30): 
     print("The temperatur is " + str(temp) + "C, it's a bad day\ndon't go outside please")
