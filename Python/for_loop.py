0a381dfcb944488c3602695564d0467e82a031cc
Ryo Richie <ryorichie@gmail.com>
2023-02-14 10:20:37 +0700

    done work in for_loop

---
+++ Python/for_loop.py
@@ -0,0 +1,20 @@
+import time
+# while loop = unlimeted
+# for loop = limited
+
+for i in range(10):
+    print(i+1)
+
+for i in range(50, 101, 2): #(start, stop, step)
+    print(i)
+
+for i in "Ryo Richie":
+    print(i)
+
+for i in range (10, 0, -1):
+    print(i)
+    time.sleep(1)
+print("Happy New Year!")
+
+for i in range(10):
+    print(i)
