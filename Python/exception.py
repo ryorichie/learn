7a0dcadff43f0a84ca6e9a89b75ad726c7bd7ee1
Ryo Richie <ryorichie@gmail.com>
2023-02-22 09:01:28 +0700

    make exception

---
+++ Python/exception.py
@@ -0,0 +1,8 @@
+try:
+    numerator = int(input("Enter a number to divide : "))
+    denominator = int(input("Enter a number to divide by : "))
+    result = numerator/denominator
+    print(result)
+except ZeroDivisionError
+except Exception:
+    print("something went wrong :(")
