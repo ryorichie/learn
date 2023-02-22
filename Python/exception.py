372966e86195d2c9ddf68adb4334fae114551ad3
Ryo Richie <ryorichie@gmail.com>
2023-02-22 11:05:21 +0700

    done

--- Python/exception.py
+++ Python/exception.py
@@ -3,6 +3,13 @@ try:
     denominator = int(input("Enter a number to divide by : "))
     result = numerator/denominator
     print(result)
-except ZeroDivisionError
-except Exception:
+except ZeroDivisionError as e:
+    print(e)
+    print("Don't use zero")
+except ValueError as e:
+    print(e)
+    print("Just use numbers")
+except Exception as e:
+    print(e)
     print("something went wrong :(")
+
