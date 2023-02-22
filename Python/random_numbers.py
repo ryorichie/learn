0ecc4ab23088da851fbabf10bd3627e648896a58
Ryo Richie <ryorichie@gmail.com>
2023-02-22 08:28:02 +0700

    done random numbers

--- Python/random_numbers.py
+++ Python/random_numbers.py
@@ -1,10 +1,15 @@
 import random
 
-x = random.randint(1,6)
+x = random.randint(1, 6)
 y = random.random()
 
-myList = ["Rock", "Paper", "Scissor"]
+myList = ['Rock', 'Paper', 'Scissor']
+cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'J', 'Q', 'K', 'A']
+z = random.choices(myList)
+random.shuffle(cards)
+print(cards)
 
-print(myList[random.randint(0,2)])
+print(myList[random.randint(0, 2)])
 print(x)
 print(y)
+print(z)
