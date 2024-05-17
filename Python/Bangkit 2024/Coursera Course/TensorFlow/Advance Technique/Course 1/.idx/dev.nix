0c7bf559607aa70c5c483d02c601c833714670f3
ryorichie <ryorichie@gmail.com>
2024-05-17 06:05:32 +0000

    oke lah

--- Python/Bangkit 2024/Coursera Course/TensorFlow/Advance Technique/Course 1/.idx/dev.nix
+++ Python/Bangkit 2024/Coursera Course/TensorFlow/Advance Technique/Course 1/.idx/dev.nix
@@ -8,12 +8,14 @@
   packages = [
     # pkgs.go
     pkgs.python311
-    pkgs.python311Packages.pip
-    pkgs.python311Packages.tensorflow
-    pkgs.python311Packages.ipykernel
-    pkgs.python311Packages.jupyter
+    # pkgs.python311Packages.pip
     # pkgs.nodejs_20
     # pkgs.nodePackages.nodemon
+    # pkgs.python311Packages.ipykernel
+    # pkgs.python311Packages.jupyter
+    pkgs.wget
+    pkgs.python3
+    pkgs.conda
   ];
 
   # Sets environment variables in the workspace
@@ -21,7 +23,15 @@
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
-      # "vscodevim.vim"
+      "vscodevim.vim"
+      "odeium.codeium"
+      "ms-python.debugpy"
+      "ms-python.python"
+      "ms-toolsai.jupyter"
+      "ms-toolsai.jupyter-keymap"
+      "ms-toolsai.jupyter-renderers"
+      "ms-toolsai.vscode-jupyter-cell-tags"
+      "ms-toolsai.vscode-jupyter-slideshow"
     ];
 
     # Enable previews
@@ -46,8 +56,9 @@
       # Runs when a workspace is first created
       onCreate = {
         # Example: install JS dependencies from NPM
-        # npm-install = 'npm install';
+        # npm-install = "npm install";
       };
+      # Runs when the workspace is (re)started
       onStart = {
         # Example: start a background task to watch and re-build backend code
         # watch-backend = "npm run watch-backend";
